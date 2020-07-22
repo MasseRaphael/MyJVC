274911c48c8a283530b35b22254b66394b5083b2
MasseRaphael <38262822+MasseRaphael@users.noreply.github.com>
2020-07-22 15:47:49 +0200

    création controller et template

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,22 @@
+<?
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AccueilController extends AbstractController
+{
+    /**
+     * @Route("/accueil", name="accueil")
+     */
+    public function index()
+    {
+        return $this->render('accueil/index.html.twig',
+        [
+            
+        ])
+    }
+
+}
