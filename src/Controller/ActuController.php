1add61a005396320ed1708f4b3587ea95e606ede
MasseRaphael <38262822+MasseRaphael@users.noreply.github.com>
2020-08-12 23:01:53 +0200

    intégration style jvc

--- src/Controller/ActuController.php
+++ src/Controller/ActuController.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class CreationController extends AbstractController
+class ActuController extends AbstractController
 {
     /**
      * @Route("/Actu", name="actu")
