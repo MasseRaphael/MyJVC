b17542ff42c9f24798d93d731daf9bac6b00d84e
MasseRaphael <38262822+MasseRaphael@users.noreply.github.com>
2020-08-12 11:46:28 +0200

    Crétion des pages à réaliser

---
+++ src/Controller/ConnexionController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ConnexionController extends AbstractController
+{
+    /**
+     * @Route("/connexion", name="connexion")
+     */
+    public function index()
+    {
+        
+    }
+
+}
---
+++ src/Controller/GestionController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class GestionController extends AbstractController
+{
+    /**
+     * @Route("/gestion", name="gestion_article")
+     */
+    public function index()
+    {
+        
+    }
+
+}
