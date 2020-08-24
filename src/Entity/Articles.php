8c82fce08db186e6c4843187169065764373cc82
MasseRaphael <38262822+MasseRaphael@users.noreply.github.com>
2020-08-25 01:11:48 +0200

    suppression des slug en base de donnée

--- src/Entity/Articles.php
+++ src/Entity/Articles.php
@@ -48,11 +48,6 @@ class Articles
      */
     private $users;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $slug;
-
     public function getId(): ?int
     {
         return $this->id;
@@ -130,15 +125,4 @@ class Articles
         return $this;
     }
 
-    public function getSlug(): ?string
-    {
-        return $this->slug;
-    }
-
-    public function setSlug(string $slug): self
-    {
-        $this->slug = $slug;
-
-        return $this;
-    }
 }
