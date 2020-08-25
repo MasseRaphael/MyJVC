12bbad7c498d19b070088eef973d9a028cf116b7
MasseRaphael <38262822+MasseRaphael@users.noreply.github.com>
2020-08-25 22:01:20 +0200

    recréation de l'ensemble des gestion d'article avec la commande make:crud

--- src/Form/ArticlesType.php
+++ src/Form/ArticlesType.php
@@ -7,14 +7,17 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class AjoutArticleFormType extends AbstractType
+class ArticlesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('titre')
             ->add('corps')
+            ->add('created_at')
+            ->add('updated_at')
             ->add('featured_image')
+            ->add('users')
             ->add('categories')
         ;
     }
