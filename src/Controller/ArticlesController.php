ddda4199a07b1d77840f64d354ec8a5244832178
MasseRaphael <38262822+MasseRaphael@users.noreply.github.com>
2020-08-25 21:41:00 +0200

    création controller pour les articles

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Articles;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class ArticlesController
+ * @package app\Controller
+ * @Route("/actualites", name="actualites_")
+ */
+
+class ArticlesController extends AbstractController
+{
+    /**
+     * @Route("/", name="articles")
+     */
+    public function index()
+    {
+
+        $articles = $this->getDoctrine()->getRepository(Articles::class)->findAll();
+
+        return $this->render('articles/index.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+}
