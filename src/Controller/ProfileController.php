732fa7574159d4c771c9c6f9aa4b68febb9646c6
MasseRaphael <38262822+MasseRaphael@users.noreply.github.com>
2020-08-27 12:00:04 +0200

    création d'une page profile pour utilisateur

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Articles;
+use App\Entity\Users;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
 {
     /**
-     * @Route("/profile", name="profile")
+     * @Route("/profile/{username}", name="profile")
      */
-    public function index()
+    public function index($username)
     {
+        $users = $this->getDoctrine()->getRepository(Users::class)->findOneBy(['username' => $username]);
         return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+            'users' => $users,
         ]);
     }
 }
